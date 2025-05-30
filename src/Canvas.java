2b3a882075e8735902324785ffac3442d4e2c164
Haos <dvorkin.alon@gmail.com>
2025-05-30 14:30:38 +0300

    add shapes_indexes

--- src/Canvas.java
+++ src/Canvas.java
@@ -2,10 +2,20 @@ public class Canvas {
     private int width;
     private int height;
     private Shape[][] shapes;
+    private int[][] shapes_indexes;
+
+
 
     public Canvas(int width, int height) {
+
         this.width = width;
         this.height = height;
+
         shapes = new Shape[width][height];
+        shapes_indexes = new int[width][height];
     }
+
+
+
+
 }
