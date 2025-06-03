7e287edf3dd7e3c8ea5dbaf039c88d81efc359b8
InfinityHaosMaster <86956960+InfinityHaosMaster@users.noreply.github.com>
2025-06-03 17:17:29 +0300

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -1,93 +1,46 @@
 /**
- * An abstract class representing a shape.
- * All subclasses of Shape must implement the following defined methods.
+ * Represents a square shape, which is a special case of a rectangle
+ * where the width and height (side lengths) are equal.
  **/
-public abstract class Shape implements Cloneable{
+public class Square extends Rectangle {
 
-    /**Cell properties*/
-    protected static final String shapeCell = "*";
-    protected static final String spaceCell = " ";
-    protected static final String endLine = "\n";
-    private static final int sizeSpaceBetweenCells = 2;
-    private static final int sizeSpaceInBounds = 1;
-    protected static final String spaceBetweenCells = Shape.spaceCell.repeat( Shape.sizeSpaceBetweenCells);
-    protected static final String spaceInBound = Shape.spaceCell.repeat( Shape.sizeSpaceInBounds );
+    /**Properties of Square*/
+    private final int side;
 
-    /**Properties for analyzing shape string*/
-    private DisplayedShapeData shapeData;
 
-    /**Create {@code shapeData}
-     * <br> Note: this function can set data shape once (since shape is immutable object)
-     * @param width width of the shape
-     * @param height height of the shape
-     * @param shapeStr shape's String
-     * */
-    protected final void setShapeDataObject( int width, int height, String shapeStr ){
-        if( this.shapeData == null ){
-            this.shapeData = new DisplayedShapeData( this.getClass().getName(),
-                    width,  height,  shapeStr);
-        }
-    }
-
-    /**Get a row of shapes string
-     * @param rowNumber
-     * @return string of the row */
-    protected final String cutRowFromString( int rowNumber ){
-        return shapeData.cutRowNumber( rowNumber);
-    }
-
-    protected final int calculateRowLength( int rowNumber ){
-        return this.shapeData.calculateRowLen( rowNumber );
+    /**
+     * Constructs a square with the given side length.
+     *
+     * @param side the side length of the square.
+     */
+    public Square(int side) {
+        super( side , side );
+        this.side=side;
     }
 
     /**
-     * Returns the height of the shape.
+     * Compares this square to another object for equality.
+     * Two squares are considered equal if they are of the same class and have the same side length.
      *
-     * @return the height.
+     * @param otherObj the object to compare to.
+     * @return true if the objects are the same square with equal side length, false otherwise.
      **/
-    public abstract int getHeight();
+    @Override
+    public boolean equals(Object otherObj) {
+        if (this == otherObj) return true;
 
-    /**
-     * Returns the width of the shape.
-     *
-     * @return the width.
-     **/
-    public abstract int getWidth();
+        if (otherObj == null) return false;
 
-    /**
-     * Calculates the area of the shape.
-     *
-     * @return the area.
-     **/
-    public abstract double area();
+        if (this.getClass() != otherObj.getClass())
+            return false;
 
-    /**
-     * Calculates the perimeter of the shape.
-     *
-     * @return the perimeter.
-     **/
-    public abstract double perimeter();
-
-    /**
-     * Returns a string representation of the shape.
-     * Each shape should implement its own drawing logic using asterisks.
-     *
-     * @return a string that visually represents the shape.
-     **/
-    public abstract String toString();
-
-    /**
-     * Checks whether this shape is equal to another object.
-     * Two shapes are considered equal if they are of the same type
-     * and have the same attributes.
-     *
-     * @param obj the object to compare with this shape
-     * @return true if they are found equal, and returns false otherwise
-     **/
-    public abstract boolean equals(Object obj);
-
+        Square otherSquare= (Square) otherObj;
+        return this.getWidth() == otherSquare.getWidth();
+    }
 
     /**Clone function*/
     @Override
-    public abstract Shape clone();
+    public Square clone(){
+        return new Square( this.side );
+    }
 }
